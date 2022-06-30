3de068091b2a1e7b2fbdc642f0b24b9dff28fdda
José Ignacio Yánez <joseignacioyanez@hotmail.com>
2022-06-30 18:46:48 -0500

    Agregar Comentarios en SistemaEscolastico.py

--- PROYECTO_FINAL_G3/CODIGOS PROYECTO/Version1/SistemaEscolastico.py
+++ PROYECTO_FINAL_G3/CODIGOS PROYECTO/Version1/SistemaEscolastico.py
@@ -2,6 +2,9 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+# Hola este es un comentraio
+
 @app.route("/")
 def hello_world():
+    # Aqui necesitamos cabiar blansdafds
     return render_template('ABP.html')
